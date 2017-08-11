a17137e8c0f13bc1ae18b5d206c70669847ba1b9
Brendan Lee <brendan@52inc.com>
2017-08-10 20:31:58 -0400

    Initial Commit

---
+++ FontBuilder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FontBuilder
+//
+//  Created by Brendan Lee on 8/10/17.
+//  Copyright © 2017 Brendan Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
